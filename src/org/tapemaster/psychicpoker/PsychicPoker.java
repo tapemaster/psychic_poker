2240b0e441c11a5467a244ed43b7b85ea004fd44
tapemaster <alexei.kuznetsov@gmail.com>
2013-06-07 20:36:12 +0200

    first commit

---
+++ src/org/tapemaster/psychicpoker/PsychicPoker.java
@@ -0,0 +1,13 @@
+package org.tapemaster.psychicpoker;
+
+public class PsychicPoker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
